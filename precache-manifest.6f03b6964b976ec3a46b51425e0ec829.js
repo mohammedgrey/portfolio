0954c5e5992a72f9e172ff8a5a7277c2ce2898bd
mohammedgrey <mohammeddsaadd@yahoo.com>
2020-12-01 08:55:23 +0200

    Updates

---
+++ precache-manifest.6f03b6964b976ec3a46b51425e0ec829.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a5db541ccab5909d4d5a03d6339858c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9007f8a979c42a97ec05",
+    "url": "/portfolio/static/css/main.3fd0c2d2.chunk.css"
+  },
+  {
+    "revision": "70953202f57be9961d2f",
+    "url": "/portfolio/static/js/2.802d16ef.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.802d16ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9007f8a979c42a97ec05",
+    "url": "/portfolio/static/js/main.5b34d685.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "6e798f86fabeb014a90fdb4d635a776a",
+    "url": "/portfolio/static/media/cands.6e798f86.png"
+  },
+  {
+    "revision": "0143bb3c53ca62d9bd1de138e7afde6b",
+    "url": "/portfolio/static/media/izi.0143bb3c.png"
+  },
+  {
+    "revision": "beb014e64c2c9110d0c64e4dc48184f0",
+    "url": "/portfolio/static/media/me.beb014e6.jpg"
+  },
+  {
+    "revision": "f16ccabea7d9a4bda5ebbb9d58da797b",
+    "url": "/portfolio/static/media/spotify.f16ccabe.png"
+  }
+]);
