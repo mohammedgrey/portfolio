9c0de67d5a8e9d28105062e238bf0f24ddfe9fc4
mohammedgrey <mohammeddsaadd@yahoo.com>
2021-06-12 01:31:40 +0200

    Adjusted the styling of the Alert to be centered.

--- src/General/Alert.js
+++ src/General/Alert.js
@@ -2,10 +2,7 @@ import React from "react";
 import "./Alert.css";
 const Alert = (props) => {
   return (
-    <div
-      style={{ display: props.visible ? "block" : "none" }}
-      className={`center-alert animate__animated animate__fadeInDown`}
-    >
+    <div style={{ display: props.visible ? "block" : "none" }} className={`center-alert animate__animated animate__fadeInDown`}>
       <p>{props.message}</p>
     </div>
   );
