0b53a1942bc3f26063ed3e86d3c763129e0e1881
mohammedgrey <mohammeddsaadd@yahoo.com>
2022-04-07 00:57:44 +0200

    Updates

---
+++ precache-manifest.6ed28176dab7e8b267c8c8cb2861b152.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98df2689430067c371b111b96a7afd26",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c663d9555cf85cf83afb",
+    "url": "/portfolio/static/css/2.3fc42fd8.chunk.css"
+  },
+  {
+    "revision": "2ff1bb5bb1c79b1c63c6",
+    "url": "/portfolio/static/css/main.629361c7.chunk.css"
+  },
+  {
+    "revision": "c663d9555cf85cf83afb",
+    "url": "/portfolio/static/js/2.3d208e0d.chunk.js"
+  },
+  {
+    "revision": "28482eb98f760960ada741568b6fc24d",
+    "url": "/portfolio/static/js/2.3d208e0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ff1bb5bb1c79b1c63c6",
+    "url": "/portfolio/static/js/main.06206f27.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "0728c4e0f84909e51e1e36dc028dd278",
+    "url": "/portfolio/static/media/Datatec.0728c4e0.pdf"
+  },
+  {
+    "revision": "f186b7e35ac86e298a995ae2b8d3d8b0",
+    "url": "/portfolio/static/media/FontClassifier.f186b7e3.pdf"
+  },
+  {
+    "revision": "f7be7b0812869d5190e6a48f3ad7eaf7",
+    "url": "/portfolio/static/media/FontClassifier.f7be7b08.png"
+  },
+  {
+    "revision": "06f6c3d2539681fcdf4de0a64d6cdb31",
+    "url": "/portfolio/static/media/Grader.06f6c3d2.jpg"
+  },
+  {
+    "revision": "6bf494b7618e8c7af200ee75cd492d99",
+    "url": "/portfolio/static/media/ImageProcessing.6bf494b7.pdf"
+  },
+  {
+    "revision": "6f7ff951a1d21289c165bf31d72bf9e2",
+    "url": "/portfolio/static/media/Networks.6f7ff951.png"
+  },
+  {
+    "revision": "277197e9692f43199f13b564b3a51ffb",
+    "url": "/portfolio/static/media/Processor.277197e9.pdf"
+  },
+  {
+    "revision": "0efaa5a6ac77e65977dc69d784c4c0e8",
+    "url": "/portfolio/static/media/SearchEngine.0efaa5a6.pdf"
+  },
+  {
+    "revision": "ea46f7863bfbeea371202ef813556aa1",
+    "url": "/portfolio/static/media/background.ea46f786.jpg"
+  },
+  {
+    "revision": "50afd386e1cbbcbf200cb06c713f9fa2",
+    "url": "/portfolio/static/media/contact.50afd386.jpg"
+  },
+  {
+    "revision": "bfcd1df4785b735648ef63f57dec963d",
+    "url": "/portfolio/static/media/me.bfcd1df4.jpg"
+  },
+  {
+    "revision": "526dd73c6276e6e0a3d95a0c76d7cdcc",
+    "url": "/portfolio/static/media/santa.526dd73c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f5fedbf2b1fb5ea2c816993e207613c5.js"
+  "/portfolio/precache-manifest.6ed28176dab7e8b267c8c8cb2861b152.js"
 );
 
 self.addEventListener('message', (event) => {
